bb9c31602cfed3f2b0406941772f19ab4f0245ae
MO21016 <mo21016@ues.edu.sv>
2025-09-21 11:31:56 -0600

    Proyecto inicial con Spring Boot y Maven

---
+++ src/test/java/com/api/gestioncitasmedicas/GestioncitasmedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gestioncitasmedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncitasmedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

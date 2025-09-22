ea1c82c67e8904f7430202382c9619de594db812
Gonjricardo <gg22088@ues.edu.sv>
2025-09-21 22:41:19 -0600

    se agrego un comentario

--- src/main/java/com/api/gestioncitasmedicas/GestioncitasmedicasApplication.java
+++ src/main/java/com/api/gestioncitasmedicas/GestioncitasmedicasApplication.java
@@ -3,9 +3,11 @@ package com.api.gestioncitasmedicas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+
 @SpringBootApplication
 public class GestioncitasmedicasApplication {
-
+    /*Este es un comentario*/
 	public static void main(String[] args) {
 		SpringApplication.run(GestioncitasmedicasApplication.class, args);
 	}
